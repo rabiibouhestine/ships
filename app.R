900f938402a0c50b0ec5845cc5ccb44eceb331ea
Rabii Bouhestine <rabiibouhestine@outlook.com>
2020-10-09 20:26:48 +0100

    initial layout

--- app.R
+++ app.R
@@ -9,15 +9,31 @@ source("modules/select_ship.R")
 ships <- read_csv("data/ships.csv", n_max = 10000)
 
 shipIcon <- iconList(
-    start = makeIcon("www/ship_start_icon.png", iconWidth = 60, iconHeight = 30),
+    start = makeIcon("www/ship_start_icon.png", iconWidth = 50, iconHeight = 25),
     end = makeIcon("www/ship_end_icon.png", iconWidth = 60, iconHeight = 30)
 )
 
 ui <- semanticPage(
+    theme = "cosmo",
     title = "Ships",
-    select_ship_ui("select_ship"),
-    textOutput(outputId = "selected_ship_name"),
-    leafletOutput("ship_path_map")
+    div(class = "ui container grid",
+        div(class = "six wide column",
+            segment(class = "raised segment",
+                    select_ship_ui("select_ship")
+            ),
+            segment(class = "raised segment",
+                    h1(class = "ui header", "First header")
+            )
+        ),
+        div(class = "ten wide column",
+            segment(class = "raised segment",
+                    textOutput(outputId = "selected_ship_name")
+            ),
+            segment(class = "raised segment",
+                    leafletOutput("ship_path_map")
+            )
+        )
+    )
 )
 
 
@@ -38,9 +54,10 @@ server <- shinyServer(function(input, output, session) {
                        icon = ~shipIcon[position],
                        popup = paste0("Position: ", selected_ship_info()$position, "<br>",
                                       "Latitude: ", selected_ship_info()$LAT, "<br>",
-                                      "Longitude: ", selected_ship_info()$LON, "<br>")) %>%
+                                      "Longitude: ", selected_ship_info()$LON, "<br>",
+                                      "Time of observation: ", selected_ship_info()$DATETIME)) %>%
             addPolylines(lng = ~LON, lat = ~LAT,
-                         weight = 3, dashArray = "5",
+                         weight = 2, dashArray = "5",
                          label = paste0("Distance: ", selected_ship_info()$dist_since_last_obs[1] , " m"),
                          labelOptions = labelOptions(permanent = TRUE))
     })
